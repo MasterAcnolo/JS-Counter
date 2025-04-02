eae0ddec57e8bd71faebe41739e505faf9bd13e3
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2025-04-02 14:01:56 +0200

    fix

--- src/script.js
+++ src/script.js
@@ -1,10 +1,9 @@
 function countToTen() {
     var count = 0;
-    const NUMBER = 10; // Number of repetitions, change it to write more INT Number
-
-    while (count < NUMBER) {
-
+    const NUMBER = 100; // Number of repetitions, change it to write more INT Number
+    
+    while (count < NUMBER + 1) {
         document.getElementById("theCount").innerHTML += count + "<br>";
-
+        count ++;
     }
 }
